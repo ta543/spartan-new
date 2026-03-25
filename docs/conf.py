da20aec3f91a188ceae323691dd2e5ffa1eeefb5
ta543 <96339034+ta543@users.noreply.github.com>
2026-03-25 14:09:14 +0100

    Add minimal Sphinx docs config for Read the Docs build

---
+++ docs/conf.py
@@ -0,0 +1,9 @@
+project = 'spartan-new'
+copyright = '2026'
+author = 'spartan-new contributors'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'alabaster'
